d364bcecdfcbccb3758fdee0dc400c82a2467180
MikeMathews123 <juggernaut9434@gmail.com>
2021-06-04 09:34:36 -0700

    added array #10 for value_test

--- parse-jsons-for-pm.js
+++ parse-jsons-for-pm.js
@@ -117,35 +117,14 @@ function exist_test(json) {
     return testArr;
 }
 
-// valuesToArray(json);
-// returns array of values from the k-keys.
-function valuesToArray(obj) {
-
-    // find all the keys that have direct values
-    let k = fix_f(f(obj));
-    let v = [];
-
-    // build a new string
-    let evaled = '';
-    for(const key of k) {
-        // break it down for some reason
-        let str = 'obj'.concat('.', key);
-        // definite resolution of evaled as String
-        evaled = eval(str, String);
-        // add evaled to array of Values.
-        v.push(evaled);
-    }
-    return v;
-}
-
 // valueTest(json);
 // string pm tests for each key value pair
-function value_test(obj) {
+function value_test(json) {
     let strArr = [];
-    let k = fix_f(f(obj));
-    let v = valuesToArray(obj);
+    let k = fix_f(f(json));      // key
     for(let i=0;i<k.length;i++)
     {
+        let v = eval(`json${k[i]}`);
         // split the path into pieces by .
         let s = `${k[i]}`.split('.');
         // put all except the last back together
@@ -153,36 +132,52 @@ function value_test(obj) {
         let last = s[s.length-1];
 
         // if no children, remove the extra dot.
-        if(k[i].split('.').length == 1 && (v[i] == null || typeof(v[i]) == "object" || isDate(v[i])))
+        if(k[i].split('.').length == 1 && (v == null || typeof(v) == "object" || isDate(v)))
         {
                 // make the test
-                strArr.push(`pm.test('${k[i]}', () => {\
-                    \n\tpm.expect(response).to.have.property('${k[i]}');\n});`);
+                k[i].startsWith('[') ? 
+                    strArr.push(`pm.test('${k[i]}', () => {\
+                        \n\tpm.expect(response${k[i]}).to.exist;\n});`):
+                    strArr.push(`pm.test('${k[i]}', () => {\
+                        \n\tpm.expect(response).to.have.property('${k[i]}');\n});`);
         }
 
         // if result is null or object or date
-        else if(v[i] == null || typeof(v[i]) == "object" || isDate(v[i]))
+        else if(v == null || typeof(v) == "object" || isDate(v))
         {
             // make the test
-            strArr.push(`pm.test('${k[i]}', () => {\
-                \n\tpm.expect(response.${first}).to.have.property('${last}');\n});`);
+            k[i].startsWith('[') ? 
+                strArr.push(`pm.test('${k[i]}', () => {\
+                    \n\tpm.expect(response${first}).to.have.property('${last}');\n});`):
+                strArr.push(`pm.test('${k[i]}', () => {\
+                    \n\tpm.expect(response.${first}).to.have.property('${last}');\n});`);
         }
         // if the value is not a String, remove the quotations
         else if(Number(v[i]).toString() != "NaN")
-            strArr.push(`pm.test('${k[i]}', () => {\
-                \n\tpm.expect(response.${first}).to.have.property('${last}', ${v[i]});\n});`);
+        {
+            k[i].startsWith('[') ?
+                strArr.push(`pm.test('${k[i]}', () => {\
+                    \n\tpm.expect(response${first}).to.have.property('${last}', ${v});\n});`):
+                strArr.push(`pm.test('${k[i]}', () => {\
+                    \n\tpm.expect(response.${first}).to.have.property('${last}', ${v});\n});`);
+        }
         else
-            strArr.push(`pm.test('${k[i]}', () => {\
-               \n\tpm.expect(response.${first}).to.have.property('${last}', '${v[i]}');\n});`);
+        {
+            k[i].startsWith('[') ?
+                strArr.push(`pm.test('${k[i]}', () => {\
+                    \n\tpm.expect(response${first}).to.have.property('${last}', '${v}');\n});`):
+                strArr.push(`pm.test('${k[i]}', () => {\
+                \n\tpm.expect(response.${first}).to.have.property('${last}', '${v}');\n});`);
+        }
     }
     return strArr;
 }
 
 // use case examples
-const et = exist_test(data);
-et.forEach( (str) => { console.log(str); });
-//const vt = value_test(data);
-//vt.forEach( (str) => { console.log(str); });
+//const et = exist_test(data);
+//et.forEach( (str) => { console.log(str); });
+const vt = value_test(data);
+vt.forEach( (str) => { console.log(str); });
 
 // exports
 module.exports = {exist_test, value_test};
